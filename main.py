7aa9414d56f713986685d447457b755159bcd565
aishwinj04 <aishwin@gmail.com>
2025-01-01 15:05:06 -0500

    Initialize Output

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ def analyze_video():
 
     return width, height
 
+def initialize_output():
+    
+    # create blank video to add frames to
+
+    width, height = analyze_video()
+    output = cv2.VideoWriter('assets/output.mp4', cv2.VideoWriter_fourcc(*'mp4'), 30, (width, height))
